c1cfeb8be4fd52ed965d546548e0720f0cd202e0
Honey Sonwani <honeysonwani88@gmail.com>
2020-07-21 21:49:34 +0530

    Validate title and desc when validateInput effect is received

--- app/src/main/java/honey/noteapp/addNote/AddNoteEffectHandler.kt
+++ app/src/main/java/honey/noteapp/addNote/AddNoteEffectHandler.kt
@@ -3,20 +3,45 @@ package honey.noteapp.addNote
 import com.spotify.mobius.Connectable
 import com.spotify.mobius.Connection
 import com.spotify.mobius.functions.Consumer
-import honey.noteapp.listOfNotes.UiActions
+import honey.noteapp.addNote.AddNoteEffect.ValidateInput
+import honey.noteapp.addNote.AddNoteEvent.ValidationFailed
+import honey.noteapp.addNote.AddNoteEvent.ValidationSucceeded
+import honey.noteapp.addNote.ValidationErrors.DescriptionBlank
+import honey.noteapp.addNote.ValidationErrors.TitleBlank
 
-class AddNoteEffectHandler(
-    private val uiActions: UiActions
-) : Connectable<AddNoteEffect, AddNoteEvent> {
+class AddNoteEffectHandler : Connectable<AddNoteEffect, AddNoteEvent> {
     override fun connect(output: Consumer<AddNoteEvent>): Connection<AddNoteEffect> {
         return object : Connection<AddNoteEffect> {
             override fun accept(effects: AddNoteEffect) {
+                when (effects) {
+                    is ValidateInput -> validateInput(effects, output)
+
+                }
             }
 
             override fun dispose() {
             }
 
         }
+    }
+
+    private fun validateInput(effect: ValidateInput, events: Consumer<AddNoteEvent>) {
+        val title = effect.title
+        val description = effect.desc
+
+        val validationErrors = when {
+            title.isEmpty() -> setOf(TitleBlank)
+            description.isEmpty() -> setOf(DescriptionBlank)
+            title.isEmpty() && description.isEmpty() -> setOf(TitleBlank, DescriptionBlank)
+            else -> emptySet()
+        }
+
+        val validationEvent = if (validationErrors.isEmpty()) {
+            ValidationSucceeded
+        } else {
+            ValidationFailed(validationErrors)
+        }
+        events.accept(validationEvent)
     }
 
 }
--- app/src/test/java/honey/noteapp/addNote/AddNoteEffectHandlerTest.kt
+++ app/src/test/java/honey/noteapp/addNote/AddNoteEffectHandlerTest.kt
@@ -1,7 +1,62 @@
 package honey.noteapp.addNote
 
-import org.junit.Assert.*
+import com.spotify.mobius.Connection
+import com.spotify.mobius.test.RecordingConsumer
+import honey.noteapp.addNote.AddNoteEffect.ValidateInput
+import honey.noteapp.addNote.AddNoteEvent.ValidationFailed
+import honey.noteapp.addNote.ValidationErrors.DescriptionBlank
+import honey.noteapp.addNote.ValidationErrors.TitleBlank
+import org.junit.After
+import org.junit.Test
 
 class AddNoteEffectHandlerTest {
 
+    private val receivedEvents = RecordingConsumer<AddNoteEvent>()
+    private lateinit var connection: Connection<AddNoteEffect>
+
+    @After
+    fun removeDown() {
+        connection.dispose()
+    }
+
+    @Test
+    fun `when validate input effect is received, validate entered title`() {
+        //given
+        setUpConnection()
+
+        //when
+        val effect = ValidateInput(
+            title = "",
+            desc = "this is a description"
+        )
+        connection.accept(effect)
+
+        //then
+        val expectedErrors = setOf(TitleBlank)
+        receivedEvents.assertValues(ValidationFailed(expectedErrors))
+    }
+
+
+    @Test
+    fun `when validate input effect is received, validate entered description `(){
+        //given
+        setUpConnection()
+
+        //when
+        val effect = ValidateInput(
+            title = "hello there",
+            desc = ""
+        )
+        connection.accept(effect)
+
+        //then
+        val expectedErrors = setOf(DescriptionBlank)
+        receivedEvents.assertValues(ValidationFailed(expectedErrors))
+    }
+
+    private fun setUpConnection() {
+        val addNoteEffectHandler = AddNoteEffectHandler()
+        connection = addNoteEffectHandler.connect(receivedEvents)
+    }
 }
+
